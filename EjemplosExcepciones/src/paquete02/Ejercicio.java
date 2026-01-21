d248104a8cb069012466e0eefcd3b86b5ba2b0f9
teoperalta <teo.peralta.2007@gmail.com>
2026-01-21 12:38:12 -0500

    Ejercicio

--- EjemplosExcepciones/src/paquete02/Ejercicio.java
+++ EjemplosExcepciones/src/paquete02/Ejercicio.java
@@ -12,40 +12,46 @@ import java.util.Scanner;
  * @author Usuario iTC
  */
 public class Ejercicio {
-    
-       public static void main(String[] args) {
-           int valor1;
-           int valor2;
-           int suma;
-           valor1 = obtenerValores();
-           valor2 = obtenerValores();
-           suma = obtenerSuma(valor1, valor2);
-       }
-       public static int obtenerValores(){
+
+    public static void main(String[] args) {
+        int valor1;
+        int valor2;
+        int suma;
+        valor1 = obtenerValores();
+        valor2 = obtenerValores();
+        suma = obtenerSuma(valor1, valor2);
+        imprimirReporte(valor1, valor2, suma);
+    }
+
+    public static int obtenerValores() {
         boolean bandera = true;
         int valor = 0;
-        while (bandera){
-        try{
-            Scanner entrada = new Scanner(System.in);
-            System.out.println("Ingrese valor 1:");
-            valor = entrada.nextInt(); 
-            if (valor > 0 && valor % 2 == 0){
-            bandera = false;    
+        while (bandera) {
+            try {
+                Scanner entrada = new Scanner(System.in);
+                System.out.println("Ingrese valor:");
+                valor = entrada.nextInt();
+                if (valor > 0 && valor % 2 == 0) {
+                    bandera = false;
+                }else{
+                    throw new Exception("Número inválido");
+                }
+            } catch (Exception e) {
+
+               System.out.printf(" Lo sentimos hay un error de tipo %s\n", e);
+
             }
         }
-
-        catch(Exception e){
-            
-            System.out.printf(" Lo sentimos solo valores enteros y pares");
-        
-        }
-        }
-    return valor;           
+        return valor;
     }
-       public static int obtenerSuma (int a, int b){
-           int resultado;
-           resultado = a + b;
-           return resultado;
-       }
-}
 
+    public static int obtenerSuma(int a, int b) {
+        int resultado;
+        resultado = a + b;
+        return resultado;
+    }
+
+    public static void imprimirReporte(int a, int b, int rta) {
+        System.out.printf("La suma de los numeros pares %d y %d es: %d\n", a, b, rta);
+    }
+}
